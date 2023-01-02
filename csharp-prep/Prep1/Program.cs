e9694c901351822f568010f4a39e1532c153be16
dianaxio <athina_7777@hotmail.com>
2023-01-02 18:44:56 -0300

    Added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their name. 
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
         
