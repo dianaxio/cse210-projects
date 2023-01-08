e5bad310228db18c2116c54df3a701ceeae09593
dianaxio <athina_7777@hotmail.com>
2023-01-07 23:50:45 -0300

    task completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,63 @@
 using System;
 
-class Program
+namespace Prep3
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {   
+            // For Parts 1 and 2, where the user specified the number...
+            // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+            
+            // For Part 3, where we use a random number
+            Console.Clear();
+            Random randomGenerator = new Random();
+            bool a = true;
+            
+            // We could also use a do-while loop here...
+
+            while (a) {
+                int ranNum = randomGenerator.Next(1, 100);
+
+                int guess = -1;
+                int count = 0;
+
+                while (guess != ranNum) {
+                    
+                    Console.Write("What is your guess? ");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                    guess = int.Parse(Console.ReadLine());
+                    count++;
+
+                    if (ranNum > guess) {
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        Console.WriteLine("Higher");
+                    }
+                    else if (ranNum < guess) {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Lower");
+                    }
+                    else {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("You guessed it!");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine($"It took you {count} guesses.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Would you like to play again (yes/no)? ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                string resp = Console.ReadLine();
+                Console.WriteLine();
+
+                if (resp == "no"){
+                    a = false;
+                }
+            }
+            Console.WriteLine("Thank you for playing. Goodbye.");
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Prep5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Prep 5");
+
+            DisplayWelcome();
+
+            string inputName = PromptUserName();
+
+            int inputNumber = PromptUserNumber();
+
+            int squaredNumber = SquareNumber(inputNumber);
+
+            DisplayResult (inputName, squaredNumber);
+
+
+            static void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to the program!");
+            }
+
+            static string PromptUserName()
+            {
+                Console.WriteLine("What is your name? ");
+                string name = Console.ReadLine();
+                return name;
+            }
+            
+            static int PromptUserNumber()
+            {
+                Console.WriteLine("Please enter your favorite number: ");
+                int number = int.Parse(Console.ReadLine());
+                return number;
+            }
+            
+            static int SquareNumber(int number)
+            {
+                int square = number * number;
+                return square;
+            }
+
+            static void DisplayResult(string name, int square)
+            {
+                Console.WriteLine($"{name}, the square of your number is {square}.");
+            }
+        }
     }
 }
