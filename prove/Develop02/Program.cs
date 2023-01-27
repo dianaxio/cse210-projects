142cca69933220a5067e6aa3d33f1b9dd340014d
dianaxio <athina_7777@hotmail.com>
2023-01-27 13:00:27 -0300

    program completed

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,12 +1,17 @@
 using System;
 
-/* For Exceeding Requirements: I added the option in the 
-main menu to add and delete new prompts to the journal, 
-this changes are saved in a file called prompts.txt */
+/*  For Exceeding Requirements: I added the option in the 
+    main menu to add and delete new prompts to the journal, 
+    this changes are saved in a file called prompts.txt */
 
-/*  I added color to the journal program to distinguish between the questions and the answers
-    I used this webpage to know about how to handle the FileNotFoundException: https://rollbar.com/blog/csharp-filenotfoundexception/#:~:text=One%20of%20the%20most%20commonly,mismatch%20in%20the%20file%20name.
-    I used this webpage to know how to clear a list in c#: https://www.techiedelight.com/es/delete-all-items-from-a-list-in-csharp/#:~:text=Una%20soluci%C3%B3n%20simple%20y%20directa,Clear()%20m%C3%A9todo. 
+/*  I added color to the journal program to distinguish 
+    between the questions and the answers
+    
+    I used this webpage to know about how to handle the FileNotFoundException: 
+    https://rollbar.com/blog/csharp-filenotfoundexception/#:~:text=One%20of%20the%20most%20commonly,mismatch%20in%20the%20file%20name.
+    
+    I used this webpage to know how to clear a list in c#: 
+    https://www.techiedelight.com/es/delete-all-items-from-a-list-in-csharp/#:~:text=Una%20soluci%C3%B3n%20simple%20y%20directa,Clear()%20m%C3%A9todo. 
 */
 
 class Program
